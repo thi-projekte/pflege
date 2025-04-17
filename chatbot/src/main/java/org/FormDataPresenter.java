25099e19eaeef93217d1bc9155156c16a054c409
luffd <luffd@mediamarktsaturn.de>
2025-04-17 20:05:45 +0200

    fix: LocalDate wird korrekt mit ObjectMapper serialisiert

--- chatbot/src/main/java/org/FormDataPresenter.java
+++ chatbot/src/main/java/org/FormDataPresenter.java
@@ -2,17 +2,19 @@ package org;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class FormDataPresenter {
-    public String present(FormData formData) {
 
-        ObjectMapper mapper = new ObjectMapper();
+    @Inject
+    ObjectMapper mapper;
+
+    public String present(FormData formData) {
         try {
             return mapper.writeValueAsString(formData);
         } catch (Exception e) {
             return "Could not convert formData to JSON: " + e.getMessage();
         }
-
     }
 }
