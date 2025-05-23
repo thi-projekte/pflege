10b42b71d1b101baba06228bb451bba26caa1abe
Eduard Merker <Eduard.Merker@web.de>
2025-05-23 16:54:36 +0200

    :tada: React+Tailwind4+TestLoginKomponente ShadcnUI

---
+++ frontend-sozi/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { LoginForm } from './components/login-form'
+
+function App() {
+
+  return (
+    <>
+      <LoginForm className="w-full max-w-sm mx-auto mt-10" />
+ 
+    </>
+  )
+}
+
+export default App
