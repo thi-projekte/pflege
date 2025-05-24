15ba63f8ed8cbb14a357a5bd0266c22d19122561
luffd <luffd@mediamarktsaturn.de>
2025-05-24 11:46:45 +0200

    Offene Anfragen Tabelle + react-router

--- frontend-sozi/src/App.jsx
+++ frontend-sozi/src/App.jsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { LoginForm } from './components/login-form'
+import OverviewTable from './components/table/table'
 
 function App() {
 
   return (
-    <>
-      <LoginForm className="w-full max-w-sm mx-auto mt-10" />
- 
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginForm className="w-full max-w-sm mx-auto mt-10" />} />
+        <Route path="/home" element={<OverviewTable />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
