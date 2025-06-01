9fcbdfa368b63d2217c42ff06a9299384b555feb
luffd <luffd@mediamarktsaturn.de>
2025-06-01 23:59:56 +0200

    App.jsx mit Routes

--- frontend-sozi/src/App.jsx
+++ frontend-sozi/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import keycloakService from './services/keycloakService'
-import OverviewTable from './components/table/table'
-import Header from './components/header/header'
-import LoaderCentered from './components/loader/Loader'
-import Login from './components/login/login'
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
+import keycloakService from './services/keycloakService';
+import OverviewTable from './components/table/table';
+import Header from './components/header/header';
+import LoaderCentered from './components/loader/Loader';
+import Login from './components/login/login';
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
@@ -33,19 +33,35 @@ function App() {
 
   return (
     <BrowserRouter>
-      {authenticated ? (
-        <>
-         <Header />
-          <OverviewTable />
-        </>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<OverviewTable />} />
-        </Routes>
-      )}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            authenticated ? (
+              <>
+                <Header />
+                <OverviewTable />
+              </>
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            authenticated ? (
+              <Navigate to="/" replace />
+            ) : (
+              <Login />
+            )
+          }
+        />
+        <Route path="*" element={<Navigate to={authenticated ? "/" : "/login"} replace />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend-sozi/src/components/login/login.jsx
+++ frontend-sozi/src/components/login/login.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import keycloakService from "@/services/keycloakService";
+import Logo from "@/assets/logo.svg";
 
 export default function Login() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
+      <img src={Logo} alt="Pflegital Logo" className="mb-4 w-32 h-32" />
       <h1 className="text-2xl mb-6">Willkommen bei Pflegital</h1>
       <Button 
         className="w-full max-w-sm" 
