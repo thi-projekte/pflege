f5a84427a75e984b88d06c4252850f95fabd95c1
danibl99 <blechinger.da@gmail.com>
2025-06-14 21:44:01 +0200

    Prozess WhatsAppID

---
+++ pflege-prozess/src/main/java/org/acme/travels/model/Waid.java
@@ -0,0 +1,19 @@
+package org.acme.travels.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Waid {
+    
+    @JsonProperty("waid")
+    private String waid;
+
+    public void setWaid(String waid) {
+        this.waid = waid;
+    }
+
+    public String getWaid() {
+        return waid;
+    } 
+
+
+}
