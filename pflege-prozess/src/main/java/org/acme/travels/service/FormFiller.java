6755c705160ab4dd929cd82682dbadba3504c014
danibl99 <blechinger.da@gmail.com>
2025-06-28 14:42:27 +0200

    debugg

--- pflege-prozess/src/main/java/org/acme/travels/service/FormFiller.java
+++ pflege-prozess/src/main/java/org/acme/travels/service/FormFiller.java
@@ -98,7 +98,7 @@ public class FormFiller {
                         }
                             */
 
-                            setField(form, "Ich beantrage", "tagesweise Verhinderungspflege");
+                            setField(form, "Ich beantrage", "tageweise Verhinderungspflege");
                             
 
 
