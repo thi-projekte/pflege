044c33909b9f2754e0f50acb745107f1f3e23848
luffd <luffd@mediamarktsaturn.de>
2025-07-01 15:49:14 +0200

    dennis email

--- pflege-prozess/src/main/java/org/acme/travels/service/MailVersandAntrag.java
+++ pflege-prozess/src/main/java/org/acme/travels/service/MailVersandAntrag.java
@@ -10,8 +10,7 @@ public class MailVersandAntrag {
     EmailService emailService;
 
     public void executeWorkItem() {
-       /*  String empfaenger = "denn.1998@web.de"; */
-          String empfaenger = "edh1579@thi.de";
+        String empfaenger = "denn.1998@web.de";
         String betreff = "Ausgefüllter Antrag auf Verhinderungspflege";
         String inhalt = "Bitte finden Sie den ausgefüllten Antrag im Anhang.";
 
