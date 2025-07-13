f83c9ee451703fc0d2a05eb1b423f40f8a72f2f1
dew1318 <167995553+dew1318@users.noreply.github.com>
2025-07-13 20:28:15 +0200

    Update EmailSendException.java

--- pflege-prozess/src/main/java/org/acme/travels/service/EmailSendException.java
+++ pflege-prozess/src/main/java/org/acme/travels/service/EmailSendException.java
@@ -1,5 +1,6 @@
 package org.acme.travels.service;
 
+
 public class EmailSendException extends RuntimeException {
     public EmailSendException(String message, Throwable cause) {
         super(message, cause);
