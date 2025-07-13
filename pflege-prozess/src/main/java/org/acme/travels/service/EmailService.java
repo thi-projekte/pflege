757ab81a7f796b88ce6afb339b1a582822cc8338
dew1318 <167995553+dew1318@users.noreply.github.com>
2025-07-13 18:46:29 +0200

    Merge branch 'main' of https://github.com/thi-projekte/pflege

--- pflege-prozess/src/main/java/org/acme/travels/service/EmailService.java
+++ pflege-prozess/src/main/java/org/acme/travels/service/EmailService.java
@@ -3,62 +3,80 @@ package org.acme.travels.service;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.mail.*;
 import jakarta.mail.internet.*;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.io.File;
 import java.util.Properties;
 
-import org.eclipse.microprofile.config.ConfigProvider;
-
 @ApplicationScoped
 public class EmailService {
 
- public void send(String empfaenger, String betreff, String inhalt) {
-    final String username = "MS_UBluNt@test-r9084zvdvx8gw63d.mlsender.net";
-    final String password = "mssp.fKSU0mO.jpzkmgq0w324059v.iUWFj3S";
+    @ConfigProperty(name = "kogito.services.email.username")
+    String username;
 
-    Properties props = new Properties();
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.smtp.host", "smtp.mailersend.net");
-    props.put("mail.smtp.port", "587");
+    @ConfigProperty(name = "kogito.services.email.password")
+    String password;
 
-    Session session = Session.getInstance(props,
-        new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
+    @ConfigProperty(name = "kogito.services.email.host")
+    String host;
 
-    try {
-        Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(username));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(empfaenger));
-        message.setSubject(betreff);
+    @ConfigProperty(name = "kogito.services.email.port")
+    int port;
 
-        // Teil 1: Text
-        MimeBodyPart textPart = new MimeBodyPart();
-        textPart.setText(inhalt);
+    @ConfigProperty(name = "kogito.services.email.ssl")
+    boolean ssl;
 
-        // Teil 2: Anhang (PDF-Datei)
-        MimeBodyPart attachmentPart = new MimeBodyPart();
+    @ConfigProperty(name = "kogito.services.email.starttls")
+    boolean starttls;
 
-        //File pdfFile = new File("target/ausgefuellter_antrag.pdf"); // <- Pfad zur PDF-Datei
-        String exportDir = ConfigProvider.getConfig().getValue("app.export.dir", String.class);
-File    pdfFile = new File(exportDir, "ausgefuellter_antrag.pdf");
-        attachmentPart.attachFile(pdfFile);
+    @ConfigProperty(name = "kogito.services.email.from")
+    String from;
 
-        // Kombiniere beides in einer MultiPart-Mail
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(textPart);
-        multipart.addBodyPart(attachmentPart);
+    @ConfigProperty(name = "app.export.dir")
+    String exportDir;
 
-        // Nachrichtinhalt setzen
-        message.setContent(multipart);
+    public void send(String empfaenger, String betreff, String inhalt) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", String.valueOf(starttls));
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", String.valueOf(port));
+        if (ssl) {
+            props.put("mail.smtp.ssl.enable", "true");
+        }
 
-        Transport.send(message);
+        Session session = Session.getInstance(props,
+            new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
 
-    } catch (Exception e) {
-        throw new RuntimeException(e);
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(empfaenger));
+            message.setSubject(betreff);
+
+            // Teil 1: Text
+            MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setText(inhalt);
+
+            // Teil 2: Anhang
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            File pdfFile = new File(exportDir, "ausgefuellter_antrag.pdf");
+            attachmentPart.attachFile(pdfFile);
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(textPart);
+            multipart.addBodyPart(attachmentPart);
+
+            message.setContent(multipart);
+
+            Transport.send(message);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Fehler beim Senden der E-Mail", e);
+        }
     }
-}
 }
